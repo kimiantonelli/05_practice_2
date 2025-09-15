f960ca48012c18837818d2a427be5dd8897314dc
Geonwoo Kim <bbosam2004@gmail.com>
2025-09-15 12:37:29 +0900

    Add files via upload

---
+++ 05_practice_2.ino
@@ -0,0 +1,21 @@
+#define PIN_LED 7
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  Serial.begin(115200);
+  while(!Serial){
+    ;
+  }
+}
+
+void loop() {
+  int toggle=0;
+  digitalWrite(PIN_LED, toggle);
+  delay(1000);
+  for(int i=0;i<11;i++){
+    toggle^=1;
+    digitalWrite(PIN_LED, toggle);
+    delay(100);
+  }
+  while(1){}
+}
